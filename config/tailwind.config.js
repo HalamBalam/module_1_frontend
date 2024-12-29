b9caa77b4a4c052e01652aa20f50df3df4dfa90d
Anton <anton@xteam.pro>
2024-12-30 01:17:40 +0300

    task 1. tailwind css

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,9 +8,26 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    colors: {
+      copper: {
+        100: "#FAD9C1",
+        200: "#F6C8A4",
+        300: "#F2B786",
+        400: "#EEA669",
+        500: "#E9944C",
+        600: "#D17F3E",
+        700: "#B96A31",
+        800: "#A15524",
+        900: "#8A4018",
+        dark: "#8A4018",
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      spacing: {
+        "8xl": "108rem",
       },
     },
   },
